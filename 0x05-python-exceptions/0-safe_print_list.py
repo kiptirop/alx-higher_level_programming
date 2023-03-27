fe6cfa1499e31299ba8bad55402ec9076d1e654c
kiptirop <augustinetirop@gmail.com>
2023-03-27 10:50:23 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             ret += 1
-        except indexError:
+        except IndexError:
             break
     print("")
     return (ret)
