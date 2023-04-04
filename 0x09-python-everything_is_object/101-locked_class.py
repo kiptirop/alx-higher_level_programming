a6154226a41d348b55b1515bb12655eda3803e11
kiptirop <augustinetirop@gmail.com>
2023-04-04 03:13:37 -0700

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,7 @@
 class LockedClass:
     __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError(f"{self.__class__.__name__} does not allow setting new attributes")
-        super().__setattr__(name, value)
+    """
+    does not allow setting new attributes but attributes called 'first_name'.
 
+    """
