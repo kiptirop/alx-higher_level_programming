7f7456e939165382b14da1ddb60cbf47623c82b9
kiptirop <augustinetirop@gmail.com>
2023-04-07 04:02:56 -0700

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines a name printing function"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints a name"""
+    # Check if first_name and last_name are strings
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    # Print the name
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
