18e84887ef8472bc6964cbd42063ff6edf5c1e01
kiptirop <augustinetirop@gmail.com>
2023-04-11 10:02:08 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""1-base_geometry module"""
+
+
+class BaseGeometry:
+    """Class that represents a base geometry object."""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
