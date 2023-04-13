6ef3c747612a858cc8e21f5ec18a81e90fad64c1
kiptirop <augustinetirop@gmail.com>
2023-04-13 05:06:43 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines a function that reads a text file"""
+
+def read_file(filename=""):
+    """Reads UTF8 and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
